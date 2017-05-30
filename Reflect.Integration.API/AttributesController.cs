e72582c1958429e36e18af7b68c361f7f829ce00
Brad Heller <brad.heller@gmail.com>
2017-05-30 13:58:04 -0700

    Add missing controller.

---
+++ Reflect.Integration.API/AttributesController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Reflect.Integration.API
+{
+    public class AttributesController : ApiController
+    {
+        public AttributesMap Get() {
+            return new AttributesMap();
+        }
+    }
+}
